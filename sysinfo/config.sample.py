08cbd378daf7f7e7e61b618f18831fdb507105f3
jonnythebard <jonnythebard@gmail.com>
2018-11-29 11:45:39 +0900

    add config.py sample

---
+++ sysinfo/config.sample.py
@@ -0,0 +1,12 @@
+database = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sysinfo',
+        'USER': 'sysinfo',
+        'PASSWORD': '123123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+secret_key = 'i5w%$(rkh-vvy6hqc2r=_z8ajd017^31r!i-6u2lfoeoyhy!78'
